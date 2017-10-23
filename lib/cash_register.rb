0f941936c8da86a5db8d262db728a1056c9f23c4
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-23 00:55:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,13 +2,9 @@
 class CashRegister
   attr_accessor :total
 
-  @@items = []
-
   def initialize(discount=0)
     @total = 0
-    if discount > 0
-      self.apply_discount
-    end
+    
   end
 
   def add_item(title, price)
