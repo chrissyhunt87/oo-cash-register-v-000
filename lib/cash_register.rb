6ed68340e7571c9e9c003143cc7e6bcb073a74b2
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-23 01:01:00 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item(title, price, *quantity=1)
+  def add_item(title, price, quantity=1)
     @total += price
     @@items << self
   end
