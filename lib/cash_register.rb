36467d63d8b2873bf24e138fb3e47fc1b5824d2d
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-23 01:06:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   end
 
   def apply_discount(discount)
-    discount_percent = @discount / 100
+    discount_percent = discount / 100
     new_total = @total * discount_percent
   end
 
